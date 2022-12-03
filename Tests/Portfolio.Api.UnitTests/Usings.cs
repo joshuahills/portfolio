f33555dbd1843269134f915e65744c47a39c8da8
Josh Hills <josh@chimpbyte.co.uk>
2022-12-03 20:24:25 +0000

    Create unit tests project

---
+++ Tests/Portfolio.Api.UnitTests/Usings.cs
@@ -0,0 +1,6 @@
+#pragma warning disable IDE0065 // Misplaced using directive
+global using NUnit.Framework;
+global using FluentAssertions;
+global using Moq;
+global using System.Diagnostics.CodeAnalysis;
+#pragma warning restore IDE0065 // Misplaced using directive
